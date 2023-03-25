ddcd99181e24ec49ba00723bc4494be4917361f3
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-25 21:20:12 +0600

    import ,export with relative path and use db as a array

--- src/Component/Cloths/Cloths.jsx
+++ src/Component/Cloths/Cloths.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Display from '../../Display'//import with relative path
+
 
 export default function Cloths() {
+ const Garments=[
+    {id:1,name:'shirt',price:1290},
+    {id:2,name:'pant',price:1330},
+    {id:3,name:'shocks',price:290},
+    {id:4,name:'t-shirt',price:690}
+ ]
+
   return (
-    <div>Cloths</div>
+    <div>
+        <h1>cloths</h1>
+  <ul>
+    {
+  Garments.map((item,index)=>
+<Display name={item.name} price={item.price} id={item.id} key={index}/>  
+  )
+    }
+  </ul>
+
+    </div>
   )
 }
---
+++ src/Display.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Display(props) {
+  return (
+    <div>
+    <h4>{props.name}</h4>
+    <h4>{props.price}</h4>
+    <h4>{props.id}</h4>
+
+
+    </div>
+  )
+}
